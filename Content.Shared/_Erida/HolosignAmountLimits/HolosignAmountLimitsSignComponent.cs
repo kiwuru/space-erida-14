c71432b571c4dfa132e331f876f83074a049bf91
vanomorodellefake <53963119+vanomorodellefake@users.noreply.github.com>
2026-07-25 19:16:28 +0300

    Atmos holoprojector rework (#382)

---
+++ Content.Shared/_Erida/HolosignAmountLimits/HolosignAmountLimitsSignComponent.cs
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2026 vanomorodellefake <vanomorodellefake29@gmail.com>
+// SPDX-License-Identifier: MIT
+
+namespace Content.Shared._Erida.HolosignAmountLimits.Components;
+
+[RegisterComponent]
+public sealed partial class HolosignAmountLimitsSignComponent : Component
+{
+    public EntityUid Overlord;
+}
